bb17daf036603e7aea71f07afb3ed83ed0be3a05
Bernice-Nkirote <bernicemwirigi@gmail.com>
2024-02-19 02:10:35 +0300

    second commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+
+print("Hello {} of age {} you live in {}".format(name, age, location))
